edff788463405b686b214b8008e47c210798f40a
onyou <onyou0031@naver.com>
2021-07-19 14:21:54 +0900

    Initial Commit

---
+++ Shared/TESTSWIFTApp.swift
@@ -0,0 +1,17 @@
+//
+//  TESTSWIFTApp.swift
+//  Shared
+//
+//  Created by a1 on 2021/07/19.
+//
+
+import SwiftUI
+
+@main
+struct TESTSWIFTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
